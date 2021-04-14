f9af5401edb51d325b7d44a26d5bafcee642e7f4
Shana Shin <sshshana@gmail.com>
2021-04-15 06:53:56 +0900

    Bootstrap layout and nav

--- web_app/routes/weather_routes.py
+++ web_app/routes/weather_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/weather_routes.py
 
-from flask import Blueprint, request, jsonify, render_template, redirect #, flash
+from flask import Blueprint, request, jsonify, render_template, redirect, flash
 
 from app.weather_service import get_hourly_forecasts
 
